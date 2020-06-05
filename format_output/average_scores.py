dc65b72a907ff9b9f701d0ef27ea587eb5e1d1b9
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-05 15:43:54 -0500

    Completed Average function to pass the test

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -1,2 +1,9 @@
 def average():
-    pass
+    # Get user input for scores
+    score1 = input("Please enter a value for score 1: ")
+    score2 = input("Please enter a value for score 2: ")
+    score3 = input("Please enter a value for score 3: ")
+    # Calculate and return average
+    return (int(float(score1))+int(float(score2))+int(float(score3)))/3
+
+
