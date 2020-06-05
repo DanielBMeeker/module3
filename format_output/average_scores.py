f796ec97acf1315a99b8d129cd58e702e7d76a01
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-05 16:15:22 -0500

    Formatted output and added docstring

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -1,9 +1,30 @@
+"""
+Program: Basic input and output
+Author: Daniel Meeker
+Date: 06/05/2020
+
+This program will demonstrate getting input
+from the user and formatting the output back
+to the user while keeping test driven development
+in mind.
+"""
+
+
 def average():
     # Get user input for scores
     score1 = input("Please enter a value for score 1: ")
     score2 = input("Please enter a value for score 2: ")
     score3 = input("Please enter a value for score 3: ")
     # Calculate and return average
-    return (int(float(score1))+int(float(score2))+int(float(score3)))/3
+    return (float(score1)+float(score2)+float(score3))/3
+
+
+# Get user information
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+age = input("Please enter your age: ")
+average_scores = average()
+# Format output and print to console
+print('{}, {} age: {} average grade: {:.2f}'.format(last_name, first_name, age, average_scores))
 
 
